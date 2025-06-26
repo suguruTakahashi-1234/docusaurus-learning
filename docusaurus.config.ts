544784c9ad89af0e85dfab5883dd5a405b724c37
suguruTakahashi-1234 <samonikura100@gmail.com>
2025-06-26 12:41:29 +0900

    feat: add GitHub Actions workflows for deployment and testing, update Docusaurus configuration and dependencies

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://localhost:3000',
+  url: 'https://sugurutakahashi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-learning/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
